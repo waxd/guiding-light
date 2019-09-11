37a2fd3b6b8a52e663c2bf7dcc20bee2d7bd1dc8
waxd <david.allen.wax@gmail.com>
2019-09-10 17:45:54 -0700

    Make the lighthouse controller an actual light producing block.

--- src/main/java/dev/waxd/lighthouse/client/LighthouseController.java
+++ src/main/java/dev/waxd/lighthouse/client/LighthouseController.java
@@ -4,12 +4,14 @@ import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.util.BlockRenderLayer;
 
+import javax.annotation.Nonnull;
+
 public class LighthouseController extends Block {
     public LighthouseController() {
-        super(Properties.create(Material.GLASS));
+        super(Properties.create(Material.GLASS).lightValue(15));
     }
 
-    @SuppressWarnings("NullableProblems")
+    @Nonnull
     @Override
     public BlockRenderLayer getRenderLayer() {
         return BlockRenderLayer.TRANSLUCENT;
